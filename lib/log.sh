d0f376d173f87c60e1b62df8ef04cedd75f87920
Pavel Egorov <contact@pavelegorov.xyz>
2026-03-20 22:13:00 +0300

    Use new color functions

--- lib/log.sh
+++ lib/log.sh
@@ -2,16 +2,7 @@
 
 set -eu
 
-BLACK="\033[0;30m"
-RED="\033[0;31m"
-GREEN="\033[0;32m"
-YELLOW="\033[0;33m"
-BLUE="\033[0;34m"
-PURPLE="\033[0;35m"
-CYAN="\033[0;36m"
-WHITE="\033[0;37m"
-
-RESET="\033[0m"
+. "$(dirname "$0")/lib/color.sh"
 
 LEVEL_SILENT=0
 LEVEL_ERROR=1
@@ -27,7 +18,6 @@ coloredMessage() {
     do
         case "$opt" in
             c) label_color="$OPTARG" ;;
-            f) output_format="$OPTARG" ;;
             l) label_text="$OPTARG" ;;
             *) exit 1;;
         esac
@@ -35,21 +25,9 @@ coloredMessage() {
     shift $((OPTIND - 1))
 
     label_color=${label_color:-$RESET}
-    output_format=${output_format:-"%b%s%b: %s\n"}
     message="$*"
 
-    if [ -t 2 ]
-    then
-        # STDERR exists, we are in terminal, can do fancy output
-
-        # shellcheck disable=SC2059
-        printf "$output_format" "$label_color" "$label_text" "$RESET" "$message" >&2
-    else
-        # STDERR does not exist, probably writing into a file -> no escape codes
-
-        # shellcheck disable=SC2059
-        printf "$output_format" "" "$label_text" "" "$message" >&2
-    fi
+    printf "%b%s%b: %s\n" "$label_color" "$label_text" "$(reset)" "$message" >&2
 }
 
 output() {
@@ -68,34 +46,34 @@ output() {
     done
     shift $((OPTIND - 1))
 
-    coloredMessage -l "${output_label:-OUTPUT}" -c "$GREEN" "$*"
+    coloredMessage -l "${output_label:-OUTPUT}" -c "$(fg green)" "$*"
 }
 
 debug() {
     if [ "$LOG_LEVEL" -ge "$LEVEL_DEBUG" ]
     then
-        coloredMessage -l "DEBUG" -c "$RESET" "$1"
+        coloredMessage -l "DEBUG" -c "$(reset)" "$1"
     fi
 }
 
 log() {
     if [ "$LOG_LEVEL" -ge "$LEVEL_INFO" ]
     then
-        coloredMessage -l "INFO" -c "$CYAN" "$1"
+        coloredMessage -l "INFO" -c "$(fg cyan)" "$1"
     fi
 }
 
 warning() {
     if [ "$LOG_LEVEL" -ge "$LEVEL_WARNING" ]
     then
-        coloredMessage -l "WARNING" -c "$YELLOW" "$1"
+        coloredMessage -l "WARNING" -c "$(fg yellow)" "$1"
     fi
 }
 
 error() {
     if [ "$LOG_LEVEL" -ge "$LEVEL_ERROR" ]
     then
-        coloredMessage -l "ERROR" -c "$RED" "$1"
+        coloredMessage -l "ERROR" -c "$(fg red)" "$1"
     fi
 }
 
